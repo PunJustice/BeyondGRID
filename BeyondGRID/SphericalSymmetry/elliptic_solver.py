a5045f42171834db6b116f2050378edb5d4d6239
PeterJames Nee <pjnee@live.ie>
2023-08-23 09:31:52 +0200

    Some more error messagers.

--- BeyondGRID/SphericalSymmetry/elliptic_solver.py
+++ BeyondGRID/SphericalSymmetry/elliptic_solver.py
@@ -6,42 +6,69 @@ import sys
 # For the following functions, one must define a mapping class to pass to the solver.
 # Seeing mappings.py for details.
 
+
 def solve_scalar_kovacs(N, mapper, eps1, eps2, eps4, tolerance):
-    if mapper.a != 2.:
+    if mapper.a != 2.0:
         sys.exit("Inner boundary must be at horizon")
     P, S, grid, first_deriv, second_deriv = ChebyshevHelper(N, mapper)
     sol = np.ones(N)
-    previous_guess = 0.8/grid
+    previous_guess = 0.8 / grid
     while np.sum(np.abs(sol)) > tolerance:
-        LHS = np.diag(grid/(2.+grid))@second_deriv+np.diag((2*(3.+grid)/((2.+grid)**2.)))@first_deriv+np.diag((12.*eps2)/(grid**6.))+np.diag((36.*eps4*previous_guess*previous_guess)/(grid**6.))
-        RHS = -48.*eps1/(grid**6.)-np.diag(grid/(2.+grid))@second_deriv@previous_guess-np.diag((2*(3.+grid)/((2.+grid)**2.)))@first_deriv@previous_guess-(12.*eps2*previous_guess)/(grid**6.)-(12.*eps4*previous_guess*previous_guess*previous_guess)/(grid**6.)
+        LHS = (
+            np.diag(grid / (2.0 + grid)) @ second_deriv
+            + np.diag((2 * (3.0 + grid) / ((2.0 + grid) ** 2.0))) @ first_deriv
+            + np.diag((12.0 * eps2) / (grid**6.0))
+            + np.diag((36.0 * eps4 * previous_guess * previous_guess) / (grid**6.0))
+        )
+        RHS = (
+            -48.0 * eps1 / (grid**6.0)
+            - np.diag(grid / (2.0 + grid)) @ second_deriv @ previous_guess
+            - np.diag((2 * (3.0 + grid) / ((2.0 + grid) ** 2.0)))
+            @ first_deriv
+            @ previous_guess
+            - (12.0 * eps2 * previous_guess) / (grid**6.0)
+            - (12.0 * eps4 * previous_guess * previous_guess * previous_guess)
+            / (grid**6.0)
+        )
 
-        LHS[0, :] = first_deriv[0,:]
-        RHS[0] = -first_deriv[0,:]@previous_guess
+        LHS[0, :] = first_deriv[0, :]
+        RHS[0] = -first_deriv[0, :] @ previous_guess
 
-
-        LHS[-1, :] = mapper.b*first_deriv[-1,:]
-        LHS[-1, -1] += 1.
-        RHS[-1] = -mapper.b*first_deriv[-1,:]@previous_guess - previous_guess[-1]
+        LHS[-1, :] = mapper.b * first_deriv[-1, :]
+        LHS[-1, -1] += 1.0
+        RHS[-1] = -mapper.b * first_deriv[-1, :] @ previous_guess - previous_guess[-1]
 
         sol = np.matmul(np_lin.inv(LHS), RHS)
         previous_guess += sol
         print(f"Magnitude of correction: {np.sum(np.abs(sol))}")
     return grid, previous_guess, P, S, first_deriv, second_deriv
 
+
 def solve_scalar_sbvp(N, mapper, eps1, eps2, eps4, tolerance):
-    if mapper.a > 2.:
+    if mapper.a > 2.0:
         sys.exit("Inner boundary must be within horizon.")
     P, S, grid, first_deriv, second_deriv = ChebyshevHelper(N, mapper)
     sol = np.ones(N)
-    previous_guess = 0.8/grid
+    previous_guess = 0.8 / grid
     while np.sum(np.abs(sol)) > tolerance:
-        LHS = np.diag((1.-2./grid))@second_deriv-np.diag((2*(1.-grid)/(grid**2.)))@first_deriv+np.diag((12.*eps2)/(grid**6.))+np.diag((36.*eps4*previous_guess*previous_guess)/(grid**6.))
-        RHS = -48.*eps1/(grid**6.)-np.diag((1.-2./grid))@second_deriv@previous_guess+np.diag((2*(1.-grid)/(grid**2.)))@first_deriv@previous_guess-(12.*eps2*previous_guess)/(grid**6.)-(12.*eps4*previous_guess*previous_guess*previous_guess)/(grid**6.)
+        LHS = (
+            np.diag((1.0 - 2.0 / grid)) @ second_deriv
+            - np.diag((2 * (1.0 - grid) / (grid**2.0))) @ first_deriv
+            + np.diag((12.0 * eps2) / (grid**6.0))
+            + np.diag((36.0 * eps4 * previous_guess * previous_guess) / (grid**6.0))
+        )
+        RHS = (
+            -48.0 * eps1 / (grid**6.0)
+            - np.diag((1.0 - 2.0 / grid)) @ second_deriv @ previous_guess
+            + np.diag((2 * (1.0 - grid) / (grid**2.0))) @ first_deriv @ previous_guess
+            - (12.0 * eps2 * previous_guess) / (grid**6.0)
+            - (12.0 * eps4 * previous_guess * previous_guess * previous_guess)
+            / (grid**6.0)
+        )
 
-        LHS[-1, :] = mapper.b*first_deriv[-1,:]
-        LHS[-1, -1] += 1.
-        RHS[-1] = -mapper.b*first_deriv[-1,:]@previous_guess - previous_guess[-1]
+        LHS[-1, :] = mapper.b * first_deriv[-1, :]
+        LHS[-1, -1] += 1.0
+        RHS[-1] = -mapper.b * first_deriv[-1, :] @ previous_guess - previous_guess[-1]
 
         sol = np.matmul(np_lin.inv(LHS), RHS)
         previous_guess += sol
--- BeyondGRID/SphericalSymmetry/mappings.py
+++ BeyondGRID/SphericalSymmetry/mappings.py
@@ -1,30 +1,38 @@
+import sys
+
 # For all classes in this file, a specifies the desired inner
 # boundary, b specifies the desired outer boundary.
 
+
 class linear:
     def __init__(self, a, b):
-        self.A = 0.5*(b-a)
-        self.B = 0.5*(b+a)
+        if b <= a:
+            sys.exit("Outer boundary b must be larger than inner boundary a")
+        self.A = 0.5 * (b - a)
+        self.B = 0.5 * (b + a)
         self.a = a
         self.b = b
 
     def rescale_grid(self, x):
-        return self.A*x+self.B
-    
-    def jacobian(self, x):
-        return 1/self.A
+        return self.A * x + self.B
 
-# C is an extra parameter for moving the density distribution of points.   
+    def jacobian(self, x):
+        return 1 / self.A
+
+
+# C is an extra parameter for moving the density distribution of points.
 class harald_scaling:
     def __init__(self, a, b, C):
-        self.A = (a+b+2.*C)/(b-a)
-        self.B = b-b*self.A+C-C*self.A
+        if b <= a:
+            sys.exit("Outer boundary b must be larger than inner boundary a")
+        self.A = (a + b + 2.0 * C) / (b - a)
+        self.B = b - b * self.A + C - C * self.A
         self.C = C
         self.a = a
         self.b = b
 
     def rescale_grid(self, x):
-        return self.B/(x-self.A)-self.C
-    
+        return self.B / (x - self.A) - self.C
+
     def jacobian(self, x):
-        return -((x-self.A)**2.)/self.B
+        return -((x - self.A) ** 2.0) / self.B
